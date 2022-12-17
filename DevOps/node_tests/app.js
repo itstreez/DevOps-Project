9581e5e807f6c1374d1cfdba345fdf9191ed183c
itstreez <85546045+itstreez@users.noreply.github.com>
2022-12-17 16:12:01 +0200

    Update app.js

--- DevOps/node_tests/app.js
+++ DevOps/node_tests/app.js
@@ -19,7 +19,7 @@ app.get('/test', function (req, res) {
         resp.on('data', function (result) {
 
             if (result == variableToAssert)  {
-                console.log("true");
+                console.log("trufe");
 
                 res.writeHead(200, {
                     'Content-Type': 'text/plain'

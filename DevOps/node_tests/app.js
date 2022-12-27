459b29ecaf1070270a1dc1da821a484064afac5b
emouhasseb <emouhasseb@liaisonedu.com>
2022-12-27 17:29:18 +0200

    tester

--- DevOps/node_tests/app.js
+++ DevOps/node_tests/app.js
@@ -12,7 +12,7 @@ router.use(function (req, res, next) {
 });
 
 app.get('/test', function (req, res) {
-    http.get('http://192.168.1.29:8082/', (resp) => {
+    http.get('http://192.168.1.38:8082/', (resp) => {
 
         resp.setEncoding('utf8');
 
